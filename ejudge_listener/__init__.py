fe16a6ccbf9746670c47dae847c6b648b90f1527
v.kovalev <v.kovalev@tinkoff.ru>
2019-01-18 12:54:44 +0300

    fix: Load settings.

--- ejudge_listener/__init__.py
+++ ejudge_listener/__init__.py
@@ -8,9 +8,10 @@ from ejudge_listener.routes import setup_routes
 
 def create_app(is_test=False):
     app = Flask(__name__)
-    app.config.from_pyfile('../settings/settings.cfg', silent=True)
     if is_test:
         app.config.from_pyfile('../settings/test-settings.cfg', silent=True)
+    else:
+        app.config.from_pyfile('../settings/settings.cfg', silent=True)
 
     db.init_app(app)
     mongo.init_app(app)
