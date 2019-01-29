f0ce21d2ffb168d6f8e62372f9fae802302c1d3a
v.kovalev <v.kovalev@tinkoff.ru>
2019-01-29 14:47:38 +0300

    style: Settings naming.

--- ejudge_listener/__init__.py
+++ ejudge_listener/__init__.py
@@ -2,17 +2,18 @@ from flask import Flask
 
 import cli
 from ejudge_listener.error_handler import register_error_handlers
+from ejudge_listener.extensions import rq, mongo
 from ejudge_listener.models import db
-from ejudge_listener.plugins import rq, mongo
 from ejudge_listener.routes import setup_routes
 
 
 def create_app(is_test=False):
     app = Flask(__name__)
+    app.config.from_pyfile('../configs/default.cfg', silent=True)
     if is_test:
-        app.config.from_pyfile('../settings/test-settings.cfg', silent=True)
+        app.config.from_pyfile('../configs/testing.cfg', silent=True)
     else:
-        app.config.from_pyfile('../settings/settings.cfg', silent=True)
+        app.config.from_pyfile('../configs/production.cfg', silent=True)
 
     db.init_app(app)
     mongo.init_app(app)
