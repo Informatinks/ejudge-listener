fc732d89f32ce188b68a7adb0c846ef146cc64ea
v.kovalev <v.kovalev@tinkoff.ru>
2019-02-06 19:00:13 +0300

    feat: Optimize migration script.

--- scripts/protocol_migration.py
+++ scripts/protocol_migration.py
@@ -1,13 +1,12 @@
 import math
 from typing import Optional
 
+from pymongo.errors import InvalidOperation
 from sqlalchemy import Column, Integer, func
+from sqlalchemy.orm import joinedload
 
 from ejudge_listener import create_app
-from ejudge_listener.exceptions import (
-    AuditNotFoundError,
-    ProtocolNotFoundError
-)
+from ejudge_listener.exceptions import AuditNotFoundError, ProtocolNotFoundError
 from ejudge_listener.extensions import mongo
 from ejudge_listener.models import EjudgeRun
 from ejudge_listener.models.base import db
@@ -27,49 +26,54 @@ class Run(db.Model):
 app = create_app()
 app.app_context().push()
 
-LIMIT_ROWS = 1_000
+LIMIT_ROWS = 1000
+LAST_ID = -1
 
 
-def get_ejudge_run(run: Run) -> Optional[EjudgeRun]:
-    ejudge_run = db.session.query(EjudgeRun) \
-        .filter(EjudgeRun.contest_id == run.ejudge_contest_id) \
-        .filter(EjudgeRun.run_id == run.ejudge_run_id) \
-        .one_or_none()
-    return ejudge_run
-
-
-def process_protocol(run: EjudgeRun, run_id: int):
+def process_protocol(run: EjudgeRun, run_id: int) -> Optional[dict]:
+    global LAST_ID
+    LAST_ID = run_id
     try:
         protocol = read_protocol(run)
         protocol['run_id'] = run_id
     except AuditNotFoundError:
-        print(f'Protocol({run.contest_id}, {run.run_id}) audit -')
+        print(f'Run({run_id}), Protocol({run.contest_id}, {run.run_id}) audit -')
     except ProtocolNotFoundError:
-        print(f'Protocol({run.contest_id}, {run.run_id}) proto -')
+        print(f'Run({run_id}), Protocol({run.contest_id}, {run.run_id}) proto -')
     else:
-        mongo.db.protocol.insert_one(protocol)
-        print(f'Protocol({run.contest_id}, {run.run_id}) +')
+        print(f'Run({run_id}), Protocol({run.contest_id}, {run.run_id}) +')
+        return protocol
 
 
-def migrate():
-    count = db.session.query(Run).count()
+def migrate(start_with_run_id: int = None):
+    global LAST_ID
+    if start_with_run_id:
+        count = db.session.query(Run).filter(Run.id > start_with_run_id).count()
+        LAST_ID = start_with_run_id
+    else:
+        count = db.session.query(Run).count()
+        LAST_ID = db.session.query(func.min(Run.id)).scalar() - 1
+
+    print(f'Script starts.. Last run_id={LAST_ID}, ids remaining={count}')
     total_chunks = math.ceil(count / LIMIT_ROWS)
-    last_id = db.session.query(func.min(Run.id)).scalar() - 1
 
     for _ in range(total_chunks):
-        runs = db.session.query(Run) \
-            .filter(Run.id > last_id) \
-            .order_by(Run.id) \
+        runs = (
+            db.session.query(EjudgeRun, Run)
+            .filter(EjudgeRun.contest_id == Run.ejudge_contest_id)
+            .filter(EjudgeRun.run_id == Run.ejudge_run_id)
+            .filter(Run.id > LAST_ID)
+            .order_by(Run.id)
             .limit(LIMIT_ROWS)
-        for run in runs:
-            ejudge_run = get_ejudge_run(run)
-            if ejudge_run is not None:
-                process_protocol(ejudge_run, run.id)
-            else:
-                msg = f'EjudgeRun({run.contest_id}, {run.run_id}) not found'
-                print(msg)
+            .options(joinedload(EjudgeRun.problem))
+        )
 
-        last_id = runs[-1].id
+        protocols = (process_protocol(ej_run, run.id) for ej_run, run in runs)
+        not_empty_protocols = filter(None, protocols)
+        try:
+            mongo.db.protocol.insert_many(not_empty_protocols)
+        except InvalidOperation:
+            pass
 
 
 if __name__ == '__main__':
