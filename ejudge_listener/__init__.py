0652fb8fac45f16da78b7936f6ab3f617d30d958
v.kovalev <v.kovalev@tinkoff.ru>
2019-02-21 15:09:35 +0300

    refactor: Refactor tasks and logging in tasks.

--- ejudge_listener/__init__.py
+++ ejudge_listener/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 import cli
 from ejudge_listener.error_handler import register_error_handlers
 from ejudge_listener.extensions import rq, mongo
+from ejudge_listener.logger import init_logger
 from ejudge_listener.models import db
 from ejudge_listener.routes import setup_routes
 
 
 def create_app():
+    init_logger()
     app = Flask(__name__)
     app.config.from_pyfile('../configs/production.cfg', silent=True)
     db.init_app(app)
